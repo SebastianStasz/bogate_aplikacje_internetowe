436324264457066c9ca862ca2fbd79dd3bb57d1b
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-04-02 11:39:03 +0200

    Add icons and set dark theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,10 +8,14 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import "@mdi/font/css/materialdesignicons.css"; 
 
 const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+        defaultTheme: 'dark'
+      }
 })
 
 createApp(App).use(vuetify).use(router).mount('#app')

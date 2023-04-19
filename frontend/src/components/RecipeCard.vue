fbd779e08fe1de8c1c60d016855a01f92d117a4a
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-04-19 17:23:27 +0200

    Recipe list: Recipe list and card setup

---
+++ frontend/src/components/RecipeCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card">
+        <img src="https://picsum.photos/390/242?random=1">
+        <div class="recipe-content">
+            <h4>{{ props.title }}</h4>
+            <p>{{ props.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    description: String
+});
+</script>
+
+<style scoped>
+.card {
+    width: 390px;
+    height: 475px;
+    border-radius: 20;
+}
+
+.recipe-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 24px;
+    gap: 12px;
+    width: 390px;
+    height: 233px;
+    background: #FFFFFF;
+    box-shadow: 0px 15px 30px rgba(224, 224, 236, 0.8);
+    border-radius: 0px 0px 20px 20px;
+}
+
+h4 {
+    font-size: 18px;
+    line-height: 27px;
+    color: black;
+}
+
+p {
+    font-size: 16px;
+    line-height: 24px;
+    color: #6B6262;
+}
+</style>
---
+++ frontend/src/components/RecipeList.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fluid>
+        <div v-for="recipe of recipes" :key="recipe.id">
+            <RecipeCard :title="recipe.title" :description="recipe.description"></RecipeCard>
+        </div>
+    </v-container>
+</template>
+
+<script setup>
+import RecipeCard from './RecipeCard.vue';
+
+const recipes = [
+    { id: 1, title: "Recipe 1", description: "Recipe 1 description.", rating: 4.5 },
+    { id: 2, title: "Recipe 2", description: "Recipe 2 description.", rating: 2.5 },
+    { id: 3, title: "Recipe 3", description: "Recipe 3 description.", rating: 4.0 },
+    { id: 4, title: "Recipe 4", description: "Recipe 4 description.", rating: 4.5 }
+]
+</script>
+
+<style scoped></style>

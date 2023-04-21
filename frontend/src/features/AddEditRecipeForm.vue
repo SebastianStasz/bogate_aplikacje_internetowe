8ba4c940e22f1df031833d79477fc7ab767eec93
PrzemekPolak <przemekpolak138@gmail.com>
2023-04-21 14:52:18 +0200

    Added routes for adding and editing recipes

---
+++ frontend/src/features/AddEditRecipeForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Tutaj formularz dodawania / edycji przepisów</div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const props = defineProps({
+  initialData: Object | String,
+});
+const formValues = reactive({
+  ingredientList: props.initialData?.ingredientList,
+});
+</script>
---
+++ frontend/src/views/EditRecipePage.vue
@@ -0,0 +1,8 @@
+<template>
+  <add-edit-recipe-form :initialData="currentData"></add-edit-recipe-form>
+</template>
+
+<script setup>
+import AddEditRecipeForm from "../features/AddEditRecipeForm.vue";
+const currentData = "tu będzie pobieranie obecnych danych przepisu z serwera";
+</script>
